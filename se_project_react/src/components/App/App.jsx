8cf1fd53ed8e3248dc58e8f70b6ee8908da89d66
clmcl96 <carsonlmclean@gmail.com>
2025-01-26 15:39:07 -0600

    Beginning the project

---
+++ se_project_react/src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import Header from "../Header/Header.jsx";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="page">
+        <div className="page__content">
+          <Header />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
